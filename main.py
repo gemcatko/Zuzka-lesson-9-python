43d0155912d071cf3fde78ccf3a06d591d13227c
zuzso <zuz.solcanska@gmail.com>
2025-01-28 19:47:14 +0100

    a tu je jemna zmena

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 with open("text.txt","w")as filehandler:
     filehandler.write("Tha zase od zaciatku\n")
     filehandler.write("A teraz este aj teraz zmenim.\n")
-    filehandler.write("toto je uplne ine ale teraz")
+    filehandler.write("toto je ine ale teraz")
 
 
 
