3d99ac3db63a713ac4ff268d82d46475fe635134
zuzso <zuz.solcanska@gmail.com>
2025-01-28 19:35:38 +0100

    zmenila som zase main a spravila som si svoju branch

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 with open("text.txt","w")as filehandler:
     filehandler.write("Tha zase od zaciatku\n")
-    filehandler.write("Bo chtoskat se mocno nudzi.\n")
+    filehandler.write("A teraz este aj teraz zmenim.\n")
     filehandler.write("toto je uplne ine ale teraz")
 
 
