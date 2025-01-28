2b1a39b3e68bbc094ab785bbcd1daef0fdcf85a2
zuzso <zuz.solcanska@gmail.com>
2025-01-28 19:03:20 +0100

    nastavil som projekt a pridal som svoj prvy kod

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+with open("text.txt","w")as filehandler:
+    filehandler.write("Tha zase od zaciatku\n")
+    filehandler.write("Bo chtoskat se mocno nudzi.\n")
+    filehandler.write("Ale ja nimam telo casu,co si!")
+
+
+
+
