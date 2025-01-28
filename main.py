10f97652e726070c1dbe658e8ee7147b318dccfa
zuzso <zuz.solcanska@gmail.com>
2025-01-28 19:08:00 +0100

    nastavil som projekt a pridal som svoj prvy kod

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 with open("text.txt","w")as filehandler:
     filehandler.write("Tha zase od zaciatku\n")
     filehandler.write("Bo chtoskat se mocno nudzi.\n")
-    filehandler.write("Ale ja nimam telo casu,co si!")
+    filehandler.write("toto je uplne ine ale teraz")
 
 
 
