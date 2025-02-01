035a22f865797e4ef7cc35e805ea1886a8194aab
zuzso <zuz.solcanska@gmail.com>
2025-02-01 23:27:38 +0100

    napisala som domacu, skusam ju commitnut a pushnut

---
+++ DU9.py
@@ -0,0 +1,14 @@
+class City:
+    def __init__(self, city_name, region_name, country_name, population, zip_code, area_code):
+        self.city_name = city_name
+        self.region_name = region_name
+        self.country_name = country_name
+        self.population = population
+        self.zip_code = zip_code
+        self.area_code = area_code
+
+    def full_address(self):
+        print(f"{self.city_name}, {self.region_name}, {self.country_name}, {self.zip_code}, {self.area_code}")
+
+Copake = City(city_name="Copake", region_name="New York", country_name="USA", population=3000, zip_code=12516, area_code=518)
+Copake.full_address()
--- vedlajsi2.py
+++ vedlajsi2.py
@@ -1,8 +1,4 @@
 
-number = float(input("Zadaj realne cislo:"))
-fractional = number - int(number)
-vysledok = str(fractional)[2]
-print(vysledok)
 
 
 
